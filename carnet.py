a1c1e787e949f43c6284a9a0c63fd9ea1c740de3
Chandana <csrinivas2@dons.usfca.edu>
2025-10-29 15:32:51 -0700

    add queries

--- carnet.py
+++ carnet.py
@@ -1,14 +1,15 @@
-from pgmpy.models import BayesianNetwork
+from pgmpy.models import DiscreteBayesianNetwork
 from pgmpy.inference import VariableElimination
 from pgmpy.factors.discrete import TabularCPD
 
-car_model = BayesianNetwork(
+car_model = DiscreteBayesianNetwork(
     [
         ("Battery", "Radio"),
         ("Battery", "Ignition"),
         ("Ignition","Starts"),
         ("Gas","Starts"),
         ("Starts","Moves"),
+        ("KeyPresent","Starts")
 ])
 
 # Defining the parameters using CPT
@@ -42,13 +43,20 @@ cpd_ignition = TabularCPD(
                  "Battery": ['Works',"Doesn't work"]}
 )
 
+cpd_key = TabularCPD(
+    variable="KeyPresent", 
+    variable_card=2, 
+    values=[[0.7], [0.3]],
+    state_names={"KeyPresent":['yes',"no"]},
+)
+
 cpd_starts = TabularCPD(
     variable="Starts",
     variable_card=2,
-    values=[[0.95, 0.05, 0.05, 0.001], [0.05, 0.95, 0.95, 0.9999]],
-    evidence=["Ignition", "Gas"],
-    evidence_card=[2, 2],
-    state_names={"Starts":['yes','no'], "Ignition":["Works", "Doesn't work"], "Gas":['Full',"Empty"]},
+    values=[[0.99, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01], [0.01, 0.99, 0.99, 0.99, 0.99, 0.99, 0.99, 0.99]],
+    evidence=["Ignition", "Gas", "KeyPresent"],
+    evidence_card=[2, 2, 2],
+    state_names={"Starts":['yes','no'], "Ignition":["Works", "Doesn't work"], "Gas":['Full',"Empty"], "KeyPresent":["yes", "no"]},
 )
 
 cpd_moves = TabularCPD(
@@ -60,12 +68,32 @@ cpd_moves = TabularCPD(
                  "Starts": ['yes', 'no'] }
 )
 
-
 # Associating the parameters with the model structure
-car_model.add_cpds( cpd_starts, cpd_ignition, cpd_gas, cpd_radio, cpd_battery, cpd_moves)
+car_model.add_cpds( cpd_starts, cpd_ignition, cpd_gas, cpd_radio, cpd_battery, cpd_moves, cpd_key)
 
 car_infer = VariableElimination(car_model)
 
 print(car_infer.query(variables=["Moves"],evidence={"Radio":"turns on", "Starts":"yes"}))
 
 
+# STEP 2:
+# Given that the car will not move, what is the probability that the battery is not working?
+print(car_infer.query(variables=["Battery"],evidence={"Moves":"no"}))
+
+# Given that the radio is not working, what is the probability that the car will not start?
+print(car_infer.query(variables=["Starts"],evidence={"Radio":"Doesn't turn on"}))
+
+# Given that the battery is working, does the probability of the radio working change if we discover that the car has gas in it?
+print(car_infer.query(variables=["Radio"], evidence={"Battery":"Works"}))
+print(car_infer.query(variables=["Radio"], evidence={"Battery":"Works", "Gas":"Full"}))
+
+# Given that the car doesn't move, how does the probability of the ignition failing change if we observe that the car does not have gas in it?
+print(car_infer.query(variables=["Ignition"], evidence={"Moves":"no"}))
+print(car_infer.query(variables=["Ignition"], evidence={"Moves":"no", "Gas":"Empty"}))
+
+# What is the probability that the car starts if the radio works and it has gas in it?
+print(car_infer.query(variables=["Starts"],evidence={"Radio":"turns on", "Gas":"Full"}))
+
+# STEP 3:
+# The probability that the key is not present given that the car does not move
+print(car_infer.query(variables=["KeyPresent"], evidence={"Moves":"no"}))
